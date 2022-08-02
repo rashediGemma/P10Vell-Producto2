e254eaa280a5120508d05c4413990528b8702eec
Gemma <gemma@rashediconsulting.de>
2022-08-02 11:12:55 +0200

    frist commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/equipo-basquet'),
+      dir: require('path').join(__dirname, './coverage/producto1'),
       subdir: '.',
       reporters: [
         { type: 'html' },
